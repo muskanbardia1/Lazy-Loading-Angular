062664e9fc698b56a73f45f0e01b5c2241eb7eaf
unknown <muskanbardia98@gmail.com>
2021-05-05 15:47:00 +0530

    lazy loading angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: 'admin', loadChildren:()=>import('./admin/admin.module')
+    .then(mod=>mod.AdminModule)
+  },
+  {
+    path: 'user', loadChildren:()=>import('./user/user.module')
+    .then(mod=>mod.UserModule)
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
